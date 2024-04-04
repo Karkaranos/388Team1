a3d99952ed0a836fd14ff44c22ce542b9ae21a6f
BBurtz <bburtz@mail.bradley.edu>
2024-04-04 13:53:55 -0500

    Color changing kickbox

--- Assets/Scripts/Player/KickBox.cs
+++ Assets/Scripts/Player/KickBox.cs
@@ -8,6 +8,8 @@ public class KickBox : MonoBehaviour
     public GameObject ball;
     public float KickStrength;
     public Vector2 BiggerSize;
+    public Color OriginalColor;
+    public Color hitColor;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -35,10 +37,17 @@ public class KickBox : MonoBehaviour
     public void Kickball()
     {
         var kickDirection = ball.transform.position - Player.transform.position;
+        gameObject.GetComponent<SpriteRenderer>().color = hitColor;
+        Invoke("fixColor", .5f);
         kickDirection.Normalize();
         ball.GetComponent<BallBehavior>().hitplayer = false;
         ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         ball.GetComponent<Rigidbody2D>().AddForce(kickDirection * KickStrength, ForceMode2D.Impulse);
         ball.GetComponent<BallBehavior>().Kicked(Player.GetComponent<Players>().currentPowerup);
+    }
+
+    public void fixColor()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = OriginalColor;
     }
 }
